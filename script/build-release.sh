3317e7efecd41216ab460aee536d0719f067dded
nikollson <nikollsonjp@gmail.com>
2026-07-15 18:15:42 +0900

    ci: publish skill linker extension artifacts

--- script/build-release.sh
+++ script/build-release.sh
@@ -8,6 +8,6 @@ mkdir -p dist
 for target in darwin-amd64 darwin-arm64 linux-amd64 linux-arm64; do
   os="${target%-*}"
   arch="${target#*-}"
-  output="dist/gh-linked-skills_${tag}_${os}-${arch}"
+  output="dist/gh-skill-linker_${tag}_${os}-${arch}"
   CGO_ENABLED=0 GOOS="$os" GOARCH="$arch" go build -trimpath -ldflags="-s -w" -o "$output" .
 done
